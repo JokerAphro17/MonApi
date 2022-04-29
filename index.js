e97df7439a9448aa2e05ee01d19f7ab5c42fd235
Ilboudo Souleymane <ilboudosouleymane4@gmail.com>
2022-04-29 15:07:42 +0000

    fin du CRUD

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const postsRoutes = require("./routes/postsController");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.use("/", postsRoutes);
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/posts", postsRoutes);
 require("./models/dbConfig");
 app.listen(5500, () => {
   console.log("Server started on port 5500");
--- routes/postsController.js
+++ routes/postsController.js
@@ -1,14 +1,70 @@
 const express = require("express");
 const router = express.Router();
+const ObjectId = require("mongodb").ObjectId;
 
 const { PostsModel } = require("../models/postsModel");
+
+// GET all posts
 router.get("/", (req, res) => {
   PostsModel.find((err, posts) => {
     if (err) {
       res.status(500).send(err);
     } else {
-      console.log(posts);
+      res.send(posts);
     }
   });
 });
+
+// Post a new post
+router.post("/", (req, res) => {
+  const newPost = new PostsModel({
+    auteur: req.body.auteur,
+    message: req.body.message,
+  });
+  newPost.save((err, post) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(post);
+    }
+  });
+});
+
+// updade a post by id
+router.put("/:id", (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    return res.status(400).send("Invalid id");
+  }
+  const updatePost = {
+    auteur: req.body.auteur,
+    message: req.body.message,
+  };
+  PostsModel.findByIdAndUpdate(
+    req.params.id,
+    { $set: updatePost },
+    { new: true },
+    (err, post) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(post);
+      }
+    }
+  );
+});
+
+// delete a post by id
+router.delete("/:id", (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    return res.status(400).send("Invalid id");
+  }
+  PostsModel.findByIdAndRemove(req.params.id, (err, post) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(post);
+    }
+  });
+});
+
 module.exports = router;
